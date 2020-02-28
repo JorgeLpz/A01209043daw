1079a6c4d3d0bfa388a18258bbd7d35e3354b0ab
Jorge López Chávez <jorge.lopez@ciasagroup.com.mx>
2020-02-28 17:02:27 -0600

    Lab6 agrega Drag and Drop

--- DiseñoWebLabs/Lab6/script.js
+++ DiseñoWebLabs/Lab6/script.js
@@ -12,16 +12,16 @@ function alerta_proyecto() {
 	let div = document.getElementById("container-alert");
 	let proyecto = document.getElementById("nombre").value;
 
-	div.innerHTML = "<div class=\"card bg-success text-white\"><div class=\"card-body\"><p>Nuevo Siniestro: "+proyecto+" </p></div></div>"
+	div.innerHTML = "<div id=\"carta_proyecto\" class=\"card bg-success text-white\" draggable=\"true\" ondragstart=\"drag(event)\"><div class=\"card-body\"><p>Nuevo Siniestro: "+proyecto+" </p></div></div>"
 
 }
 
 function agregar_producto() {
   let div = document.getElementById("consumible_nuevo");
 
-  div.innerHTML = "Cantidad:  <input id=\"numero\" value=\" \"><br> Producto:  <input id=\"descripcion\" value=\" \"> <button id=\"boton\">Registrar</button>";
+  div.innerHTML = "Cantidad:  <input id=\"numero\" value=\" \"><br> Producto:  <input id=\"descripcion\" value=\" \"> <button id=\"boton_agregar\">Registrar</button>";
 
-  document.getElementById("boton").onclick = alerta_consumible;
+  document.getElementById("boton_agregar").onclick = alerta_consumible;
 }
 
 function alerta_consumible(){
@@ -29,16 +29,16 @@ function alerta_consumible(){
   let material = document.getElementById("descripcion").value;
   let cantidad = document.getElementById("numero").value;
 
-  div.innerHTML = "<div class=\"card bg-warning text-white\"><div class=\"card-body\"><p>Se agregaron "+cantidad+" "+material+" en bodega. </p></div></div>"
+  div.innerHTML = "<div id=\"carta_consumible\" class=\"card bg-warning text-white\"draggable=\"true\" ondragstart=\"drag(event)\"><div class=\"card-body\"><p>Se agregaron "+cantidad+" "+material+" en bodega. </p></div></div>"
 }
 
 
 function prestar_articulo() {
   let div = document.getElementById("registro_prestamo");
 
-  div.innerHTML = "Cantidad:  <input id=\"numero\" value=\" \"><br> Producto:  <input id=\"descripcion\" value=\" \">Solicitante:  <input id=\"trabajador\" value=\" \">Autoriza:  <input id=\"almacenista\" value=\" \"> <button id=\"boton\">Registrar</button>";
+  div.innerHTML = "Cantidad:  <input id=\"numero\" value=\" \"><br> Producto:  <input id=\"descripcion\" value=\" \">Solicitante:  <input id=\"trabajador\" value=\" \">Autoriza:  <input id=\"almacenista\" value=\" \"> <button id=\"boton_prestar\">Registrar</button>";
 
-  document.getElementById("boton").onclick = alerta_prestamo;
+  document.getElementById("boton_prestar").onclick = alerta_prestamo;
 }
 
 function alerta_prestamo(){
@@ -48,9 +48,22 @@ function alerta_prestamo(){
   let trabajador = document.getElementById("trabajador").value;
   let empleado = document.getElementById("almacenista").value;
   
-  div.innerHTML = "<div class=\"card bg-warning text-white\"><div class=\"card-body\"><p>Se agregaron "+numero+" "+material+" prestado a "+trabajador+" autorizado por "+empleado+" en bodega. </p></div></div>"
+  div.innerHTML = "<div id=\"carta_prestamo\" class=\"card bg-warning text-white\"draggable=\"true\" ondragstart=\"drag(event)\"><div class=\"card-body\"><p>Se agregaron "+numero+" "+material+" prestado a "+trabajador+" autorizado por "+empleado+" en bodega. </p></div></div>"
 }
 
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
 
 document.getElementById("prestamo").onclick = prestar_articulo;
 
