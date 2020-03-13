73e360644ef00dd7c8c25d8ee7a7d9d846bb5acc
Jorge López Chávez <jorge.lopez@ciasagroup.com.mx>
2020-03-13 16:02:14 -0600

    Lab11 Base Datos

---
+++ Base de Datos/Laboratorio_11/bulk.sql
@@ -0,0 +1,36 @@
+BULK INSERT a1209043.a1209043.[Materiales]
+   FROM 'e:\wwwroot\rcortese\materiales.csv'
+   WITH
+      (
+         CODEPAGE = 'ACP',
+         FIELDTERMINATOR = ',',
+         ROWTERMINATOR = '\n'
+      )
+
+BULK INSERT a1209043.a1209043.[Proveedores]
+   FROM 'e:\wwwroot\rcortese\proveedores.csv'
+   WITH
+      (
+         CODEPAGE = 'ACP',
+         FIELDTERMINATOR = ',',
+         ROWTERMINATOR = '\n'
+      )
+
+BULK INSERT a1209043.a1209043.[Proyectos]
+   FROM 'e:\wwwroot\rcortese\proyectos.csv'
+   WITH
+      (
+         CODEPAGE = 'ACP',
+         FIELDTERMINATOR = ',',
+         ROWTERMINATOR = '\n'
+      )
+
+SET DATEFORMAT dmy
+BULK INSERT a1209043.a1209043.[Entregan]
+   FROM 'e:\wwwroot\rcortese\entregan.csv'
+   WITH
+      (
+         CODEPAGE = 'ACP',
+         FIELDTERMINATOR = ',',
+         ROWTERMINATOR = '\n'
+      )
